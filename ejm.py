b7e0065492943eb821db9b79e29dbb3f1dfcd8ee
Victor Serrano <victor.serrano@unl.edu.ec>
2021-08-07 17:27:45 -0500

    codigo

--- ejm.py
+++
@@ -1,9 +0,0 @@
-import os
-import pandas as pd
-from ast import literal_eval
-
-from cdqa.utils.converters import pdf_converter
-from cdqa.pipeline import QAPipeline
-from cdqa.utils.download import download_model
-
-download_model(model='bert-squad_1.1', dir='./models')
