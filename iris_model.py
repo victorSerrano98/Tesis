37bbd384109d851f13b4089fb32f0da055daf3dc
Victor Serrano <victor.serrano@unl.edu.ec>
2021-08-07 15:39:15 -0500

    Initial commit

---
+++ iris_model.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+
+
+def classify(num):
+    if num == 0:
+        return 'setosa'
+    else:
+        return 'virginica'
+
+
+def main():
+    st.title('Alexis puto')
+    st.sidebar.header('par')
+
+    def parametros():
+        pregunta=st.text_input('ingrese pregunta')
+        return pregunta
+    P = parametros()
+
+    if st.button('Aceptar'):
+        st.success('No')
+if __name__ == '__main__':
+    main()
