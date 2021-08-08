f59e31320d68022a22564458386121a478dd1f34
Victor Serrano <victor.serrano@unl.edu.ec>
2021-08-07 21:19:56 -0500

    Update iris_model.py

--- iris_model.py
+++ iris_model.py
@@ -2,11 +2,6 @@ import streamlit as st
 import pandas as pd
 import Tesis1
 
-def classify(num):
-    if num == 0:
-        return 'setosa'
-    else:
-        return 'virginica'
 
 def main():
     st.title('Chatbot:')
@@ -16,9 +11,9 @@ def main():
     if st.button('Aceptar'):
         traducido = Tesis1.traductor(pregunta)
         query = Tesis1.spa(traducido)
-        articulos = Tesis1.vespa(query)
-        respuesta = Tesis1.respuesta(traducido, articulos)
-        st.success(respuesta)
+        #articulos = Tesis1.vespa(query)
+        #respuesta = Tesis1.respuesta(traducido, articulos)
+        st.success(traducido+ "   :   " query)
 if __name__ == '__main__':
     main()
  
