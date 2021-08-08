152a4eb36059c178620252c2a1ca23c515929c8b
Victor Serrano <victor.serrano@unl.edu.ec>
2021-08-07 21:13:55 -0500

    Update iris_model.py

--- iris_model.py
+++ iris_model.py
@@ -15,10 +15,10 @@ def main():
 
     if st.button('Aceptar'):
         traducido = Tesis1.traductor(pregunta)
-        #query = Tesis1.spa(traducido)
-        #articulos = Tesis1.vespa(query)
-        #respuesta = Tesis1.respuesta(traducido, articulos)
-        st.success(pregunta)
+        query = Tesis1.spa(traducido)
+        articulos = Tesis1.vespa(query)
+        respuesta = Tesis1.respuesta(traducido, articulos)
+        st.success(respuesta)
 if __name__ == '__main__':
     main()
  
